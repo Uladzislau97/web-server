9c0972419a8fdf6a094e341e5b3d6a45aeedb450
Uladzislau <VSvasilyev97@gmail.com>
2016-08-24 19:12:19 +0300

    Add code to analyse HTTP request

--- server.rb
+++ server.rb
@@ -3,7 +3,26 @@ require 'socket'               # Get sockets from stdlib
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)  # Send the time to the client
-  client.puts 'Closing the connection. Bye!'
+
+  request = client.read
+  request_parts = request.scan(/(?i)(GET|POST) (((\/\w+)*)(\.\w+)) (HTTP\/[12]\.[01])/)
+
+  p request_parts
+
+  if request_parts.empty?
+    puts 'Wrong HTTP request.'
+  else
+    method = request_parts[0]
+    file = request_parts[1]
+
+    if method.upcase == 'GET'
+
+    elsif method.upcase == 'POST'
+
+    end
+  end
+
+  #client.puts(Time.now.ctime)  # Send the time to the client
+  #client.puts 'Closing the connection. Bye!'
   client.close                 # Disconnect from the client
 }
