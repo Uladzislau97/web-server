360f3808c99c93d628abb633389d002fe3127c82
Uladzislau <VSvasilyev97@gmail.com>
2016-08-26 15:24:14 +0300

    Add TinyWebBrowser class

--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -1,15 +1,21 @@
 require 'socket'
 
-host = 'www.tutorialspoint.com'     # The web server
-port = 80                           # Default HTTP port
-path = '/index.htm'                 # The file we want
+#path = '/content/index.htm'
 
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+#request = "GET #{path} HTTP/1.0\r\n\r\n"
 
-socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2)
-print body
+
+#socket.print(request)
+#response = socket.read
+
+#headers,body = response.split("\r\n\r\n", 2)
+#print body
+
+class TinyWebBrowser
+  def initialize
+    host = 'localhost'
+    port = 2000
+    @socket = TCPSocket.open(host,port)
+  end
+
+end
